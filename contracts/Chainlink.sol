2016ee3b14357de2fa4cb2543455dd82fe6f2baf
joshschcom <84538564+joshschcom@users.noreply.github.com>
2022-11-16 21:51:52 +0100

    Add files via upload

---
+++ contracts/Chainlink.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Chainlink is ERC20 {
+    constructor() ERC20("Chainlink", "Link") {
+        _mint(msg.sender, 5000 * 10**18);
+    }
+}
